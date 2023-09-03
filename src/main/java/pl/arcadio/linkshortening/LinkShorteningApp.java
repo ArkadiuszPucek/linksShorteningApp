70d7be288b8eafbd0e4d93d6af6c8893d94c4b78
ArkadiuszPucek <arkadiusz.pucek@gmail.com>
2023-09-03 11:35:38 +0200

    First commit of link shortening app

---
+++ src/main/java/pl/arcadio/linkshortening/LinkShorteningApp.java
@@ -0,0 +1,13 @@
+package pl.arcadio.linkshortening;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LinkShorteningApp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LinkShorteningApp.class, args);
+	}
+
+}
---
+++ src/test/java/pl/arcadio/linkshortening/LinkShorteningAppApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.arcadio.linkshortening;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LinkShorteningAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
